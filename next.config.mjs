ecc2b92a84de254d63ab19633c6fa5ecf1bcd68e
贾伦鹏 <jialunpeng@jialunpengdeMac-Studio.local>
2024-07-02 23:03:04 +0800

    feat: 完成官方新人学习

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        ppr: 'incremental', // 该'incremental'值允许您对特定路线采用 PPR。
+    },
+};
 
 export default nextConfig;
